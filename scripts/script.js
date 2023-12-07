2828c37255655b1200d9f93c3409f36b7176853e
spookybacon16 <146887302+spookybacon16@users.noreply.github.com>
2023-12-07 22:57:42 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,41 @@
 // JavaScript Document
 console.log("hi");
+
+// var openButton = document.querySelector("header > button");
+// var sluitButton = document.querySelector(".burger-menu button");
+
+
+
+// openButton.onclick = openMenu
+
+// function openMenu() {
+//     var Nav = document.querySelector(".burger-menu");
+
+//     Nav.classList.add("toonMenu");
+// }
+
+// sluitButton.onclick = sluitMenu;
+
+// function sluitMenu() {
+//     var Nav = document.querySelector(".burger-menu");
+//     Nav.classList.remove("toonMenu");
+
+// }
+
+
+
+// var burgerbutton = document.querySelector(".streepjes button");
+
+// burgerbutton.onclick = Buttonklik;
+
+// function Buttonklik() {
+//     if (burgerbutton.ariaExpanded == "false") {
+//         burgerbutton.ariaExpanded = "true"
+//     }
+
+//     else {
+//         burgerbutton.ariaExpanded = "false"
+//     }
+// }
+
+
